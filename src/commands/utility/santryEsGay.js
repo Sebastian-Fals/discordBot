cbe1841370776eed6c23152c345aa1b8fb92b7e0
Sebastian-Fals <sebastian.fals.lopez@gmail.com>
2025-05-23 13:13:27 -0500

    Easter egg upgrade3

--- src/commands/utility/santryEsGay.js
+++ src/commands/utility/santryEsGay.js
@@ -1,10 +1,12 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, userMention } = require("discord.js");
+
+const mentionId = "1099786341321035917";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("santry")
     .setDescription("Easter egg de santry"),
   async execute(interaction) {
-    await interaction.reply("@santryx_ es gay certificado");
+    await interaction.reply(userMention(mentionId) + "es gay certificado");
   },
 };
