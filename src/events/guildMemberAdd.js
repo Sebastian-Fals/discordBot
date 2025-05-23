aa6d4a0ea94f5d12e03eddc421fca74232c2e9f7
Sebastian-Fals <sebastian.fals.lopez@gmail.com>
2025-05-23 12:36:49 -0500

    Welcome message

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,47 @@
+const { Events, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: Events.GuildMemberAdd,
+  async execute(interaction) {
+    const guild = interaction.guild;
+    const channel = guild.channels.cache.find(
+      (channel) => channel.id === "1375506459625783477"
+    );
+    if (!channel) return;
+
+    const welcomeEmbed = new EmbedBuilder()
+      .setColor(0xffa500)
+      .setTitle(
+        `¡Bienvenido ${interaction.user.username} al servidor oficial de Secondor!`
+      )
+      .setDescription(
+        `
+*Donde la comunidad cobra vida y el contenido nunca se detiene.*
+
+👾 **¿Quiénes somos?**  
+Este es el punto de encuentro de la comunidad de **Secondor**, donde nos reunimos para charlar, compartir, jugar y disfrutar de todo lo relacionado con mis streams de **Twitch** y los videos de **YouTube**.
+
+📺 **¿Qué puedes encontrar aquí?**
+- 🔴 Anuncios de streams y nuevos videos  
+- 🎮 Eventos exclusivos y partidas con la comunidad  
+- 💬 Canales para charlas, memes, clips y más  
+- ⭐ Roles especiales para los más activos y suscriptores
+
+💡 **Tips para empezar:**  
+✅ Lee las reglas en \`#📜reglas\`  
+✅ Preséntate en \`#👋presentaciones\`  
+✅ Únete a la charla en \`#💬general\`  
+✅ ¡Y no olvides activar las notificaciones para no perderte nada!
+
+🚀 **Gracias por ser parte de esta aventura**  
+¡Tu presencia hace que esta comunidad sea más épica cada día!
+
+🔗 [Twitch](https://www.twitch.tv/secondor29)  
+🔗 [YouTube](https://www.youtube.com/@Secondor)
+`
+      )
+      .setThumbnail(interaction.user.displayAvatarURL());
+
+    channel.send({ embeds: [welcomeEmbed] });
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,86 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
+const CONFIG = require("../config.json");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.DirectMessagePolls,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildExpressions,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessagePolls,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.SoundboardSound,
+    Partials.ThreadMember,
+    Partials.User,
+  ],
+});
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+client.login(CONFIG.token);
